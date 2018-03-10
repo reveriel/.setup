116595f5d7d3f9ffbfdebc121e2686a5ea1644bf
Rev <reveriel@hotmail.com>
2018-03-10 12:50:15 +0800

    Update tmux.sh

--- tmux.sh
+++ tmux.sh
@@ -5,4 +5,4 @@ git clone https://github.com/gpakosz/.tmux.git
 ln -s -f .tmux/.tmux.conf
 cp .tmux/.tmux.conf.local .
 
-echo "unbind C-q \nset -g prefix C-q \nbind C-q send-prefix\n" >> ~/.tmux.conf.local
+echo "unbind C-a \nset -g prefix C-q \nbind C-q send-prefix\n" >> ~/.tmux.conf.local
