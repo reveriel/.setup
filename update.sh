4a6ee3914ccc277acda02506936d09e4b17e3458
Reveriel <reveriel@hotmail.com>
2018-11-15 01:27:10 +0000

    remove useless files

--- update.sh
+++
@@ -1,5 +0,0 @@
-git clone https://github.com/icymind/NeoSolarized.git
-git clone https://github.com/luochen1990/rainbow.git
-git clone https://github.com/majutsushi/tagbar.git
-git clone https://github.com/jpalardy/vim-slime.git
-
