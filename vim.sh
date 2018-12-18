d1993bbb44c259853fc3e0a1dc8e2f55d5209690
Reveriel <reveriel@hotmail.com>
2018-12-18 08:30:14 +0000

    update vim.sh, might be usable..

--- vim.sh
+++ vim.sh
@@ -25,15 +25,16 @@ VIMRC_DIR=$SETUP_DIR/submodules/vimrc
 function create_vimrc() {
     echo 'set runtimepath+=~/.vim_runtime
 
-    source ~/.vim_runtime/vimrcs/basic.vim
-    source ~/.vim_runtime/vimrcs/filetypes.vim
-    source ~/.vim_runtime/vimrcs/plugins_config.vim
-    source ~/.vim_runtime/vimrcs/extended.vim
+source ~/.vim_runtime/vimrcs/basic.vim
+source ~/.vim_runtime/vimrcs/filetypes.vim
+source ~/.vim_runtime/vimrcs/plugins_config.vim
+source ~/.vim_runtime/vimrcs/extended.vim
 
-    try
-    source ~/.vim_runtime/my_configs.vim
-    catch
-    endtry' > ~/.vimrc
+try
+source ~/.vim_runtime/my_configs.vim
+catch
+endtry' > ~/.vimrc
+
 }
 
 function init() {
@@ -50,18 +51,21 @@ function init() {
     git submodule update $F_GIT submodules/vimrc
 
     cd ~
-    ln -s -f $VIMRC  .vim_runtime
+    ln -s -f $VIMRC_DIR  .vim_runtime
     create_vimrc
 }
 
 function update() {
     echo "update"
-
+    cd $VIMRC_DIR
+    git sumbodule init
+    git submodule update $F_GIT
 }
 
 function main() {
     if [ $INIT ]; then
         init
+        update
     else
         update
     fi
